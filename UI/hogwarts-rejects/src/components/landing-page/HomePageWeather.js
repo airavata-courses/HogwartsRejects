5ac1bffb7efd5bd759e0c312d0f295a5c6d31329
Rishabh gajra <rgrishigajra@gmail.com>
2020-02-12 05:51:48 -0500

    weather box UI done

--- UI/hogwarts-rejects/src/components/landing-page/HomePageWeather.js
+++ UI/hogwarts-rejects/src/components/landing-page/HomePageWeather.js
@@ -6,91 +6,72 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import moment from "moment-timezone"
+import { Row, Col } from "react-bootstrap";
 function HomePageWeather(props) {
   const long = "-89";
   const lat = "39";
-  const climate = {
-    coord: { lon: -89, lat: 39 },
-    weather: [
-      { id: 804, main: "Clouds", description: "overcast clouds", icon: "04n" }
-    ],
-    base: "stations",
-    main: {
-      temp: 276.34,
-      pressure: 1023,
-      humidity: 80,
-      temp_min: 275.15,
-      temp_max: 277.59,
-      sea_level: 0,
-      grnd_level: 0
-    },
-    wind: { speed: 2.25, deg: 323 },
-    clouds: { all: 90 },
-    dt: 1581468220,
-    sys: { message: 0, country: "US", sunrise: 1581425569, sunset: 1581463668 },
-    id: 4238245,
-    name: "Fayette",
-    cod: 200
-  };
-  // const [climate, setClimate] = useState({
-  // });
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       "http://localhost:8090/climate?lon=" +
-  //         long +
-  //         "&lat=" +
-  //         lat +
-  //         "&appid=48df9480a2a09854ca753dd0721d3f64"
-  //     )
-  //     .then(function(res) {
-  //       console.log(res.data)
-  //       setClimate({
-  //         long: JSON.stringify(res.data.coord.lon),
-  //         lat: JSON.stringify(res.data.coord.lat),
-  //         weatherid:JSON.stringify(res.data.weather.id),
-  //         weathermain:JSON.stringify(res.data.main),
-  //         weatherdescription:JSON.stringify(res.data.weather.description),
-  //         temp:JSON.stringify(res.data.main.temp),
-  //         pressure:JSON.stringify(res.data.main.pressure),
-  //         humidity:JSON.stringify(res.data.main.humidity),
-  //         temp_min:JSON.stringify(res.data.main.temp_min),
-  //         temp_max:JSON.stringify(res.data.main.temp_max),
-  //         windspeed:JSON.stringify(res.data.wind.speed),
-  //         winddeg:JSON.stringify(res.data.wind.deg),
-  //         country:JSON.stringify(res.data.sys.country),
-  //         sunrise:JSON.stringify(res.data.sys.sunrise),
-  //         sunset:JSON.stringify(res.data.sys.sunset),
-  //         city:JSON.stringify(res.data.name),
-  //       })
-  //     });
-  // }, []);
+
+  const [climate, setClimate] = useState({
+  });
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:8090/climate?lon=" +
+          props.long +
+          "&lat=" +
+          props.lat +
+          "&appid=48df9480a2a09854ca753dd0721d3f64"
+      )
+      .then(function(res) {
+        console.log(res.data)
+        setClimate({
+          long: JSON.stringify(res.data.coord.lon),
+          lat: JSON.stringify(res.data.coord.lat),
+          weatherid:JSON.stringify(res.data.weather.id),
+          weathermain:res.data.weather[0].main,
+          weatherdescription:res.data.weather[0].description,
+          temp:JSON.stringify(res.data.main.temp),
+          pressure:JSON.stringify(res.data.main.pressure),
+          humidity:JSON.stringify(res.data.main.humidity),
+          temp_min:JSON.stringify(res.data.main.temp_min),
+          temp_max:JSON.stringify(res.data.main.temp_max),
+          windspeed:JSON.stringify(res.data.wind.speed),
+          winddeg:JSON.stringify(res.data.wind.deg),
+          country:res.data.sys.country,
+          sunrise:JSON.stringify(res.data.sys.sunrise),
+          sunset:JSON.stringify(res.data.sys.sunset),
+          city:res.data.name,
+          dt:JSON.stringify(res.data.dt),
+          icon:"http://openweathermap.org/img/wn/".concat(res.data.weather[0].icon,"@2x.png")
+        })
+      });
+  }, []);
+  console.log(climate.icon)
+  let timesec =new Date(0); 
+  timesec.setUTCSeconds(climate.dt)
+  const [time,setTime]=useState(JSON.stringify(timesec))
   return (
     <div>
-      <Card >
+      <Card style={{marginTop:"10vh",width:"60vh",marginLeft:"-4vh"}}>
       <CardContent>
-        <Typography  color="textSecondary" gutterBottom>
-         {climate.city}, {climate.country}
+        <Typography color="textSecondary" gutterBottom variant="h4" >
+       {climate.city}, {climate.country}
+         </Typography>
+         <Typography color="textSecondary" gutterBottom variant="h5" component="h3">
+         <strong>{climate.weathermain}</strong>
+         </Typography>
+         <div style={{display:"flex",flexDirection:"row",alignItems:"center"}}>
+        <img style={{height:"15vh",width:"15vh"}}src={climate.icon} alt="icon"/>
+       <h1 style={{display:"inline",fontSize:"7vh",marginLeft:"-2vh",paddingBottom:"1vh",position:"relative"}}>{parseInt(climate.temp-273)}<p style={{display:"inline",fontSize:"2vh",top:"-1.0vh",position:"absolute"}}>&deg;C</p></h1>
+        <Typography color="textSecondary" style={{float:"right",marginLeft:"6vh"}} >
+          Pressure: {climate.pressure} <br/>
+          Humidity: {climate.humidity} <br/>
+          Wind: {climate.windspeed} km/h, degrees: {climate.winddeg}&deg;
         </Typography>
-        <Typography variant="h5" component="h2">
-          yo
-        </Typography>
-        <Typography color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        </div>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
-      {/* {tp.coord.lon} */}
-
     </div>
   );
 }
