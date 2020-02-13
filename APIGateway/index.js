6d6f254724e79259d0a2cfb4308232a4394b4d36
Rishabh gajra <rgrishigajra@gmail.com>
2020-02-13 17:25:19 -0500

    User auth done

--- APIGateway/index.js
+++ APIGateway/index.js
@@ -1,79 +1,134 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8090;
-const version = '1.0.0';
-const bp = require('body-parser');
-const apiAdapter = require('./apiAdapter')
-const request = require('request');
-const cors = require('cors')
-const axios = require('axios');
+const version = "1.0.0";
+const bp = require("body-parser");
+const apiAdapter = require("./apiAdapter");
+const request = require("request");
+const cors = require("cors");
+const axios = require("axios");
+const uuidv4 = require('uuid/v4')
 // const isAuthorized = require('../controller/requestAuthenticator')
+
 app.use(bp.json());
-app.use(cors())
-const BASE_URL = 'http://localhost:8080'
-const api = apiAdapter(BASE_URL)
+app.use(cors());
+const BASE_URL = "http://localhost:8080";
+const api = apiAdapter(BASE_URL);
 
-var kafka = require("kafka-node")
+var kafka = require("kafka-node");
 
-app.post('/dataRetrieval',(req,res)=>{
-
-  var url=req.body.fileloc
-
-  console.log("data retrieval req with url "+req.body.fileloc)
-  
-  Producer = kafka.Producer,
-
-  client = new kafka.KafkaClient('127.0.0.1:2181'),
-  producer = new Producer(client);
-  let count = 0;
-
-producer.on("ready", function() {
-  let count = 0;
-
-  console.log("ready to write message in kafka");
-  setInterval(function() {
-    payloads = [
-      { topic: "data-retrieval", messages: url, partition: 0, key:"" }
-    ];
-
-    producer.send(payloads, function(err, data) {
-      console.log(data);
-      count += 1;
-    });
-  }, 1);
-  res.send()
-});
-
-producer.on("error", function(err) {
-  console.log(err);
-});
-})  
-
-
-app.get('/',(req,res)=>{
-  console.log("got req11111 empty / test")
-})
-app.post('/login', (req, res) => {
-  var username = req.body.email;
-  var password = req.body.password;
-  console.log(username+password)
-  
+app.post("/dataRetrieval", (req, res) => {
+  console.log("data retrieval api hit");
+  var userName
+  // console.log(req.body)
   axios
-  .post('http://localhost:8084/login',{'userName':username,'password':password})
-  .then(function (response) {
-    console.log(response.data);
-    res.send(response.data)
-  })
-  .catch(function (error) {
-    console.log(error);
+    .post("http://localhost:8084/verify", {
+      jwt: req.body.jwtToken,
+        })
+    .then(function(response) {
+      console.log(response.data.userName);
+      userName=response.data.userName
+    
+  const jobid=uuidv4();
+  console.log(jobid,userName);
+  var url = req.body.fileloc;
+  console.log("data retrieval req with url " + req.body.fileloc);
+  (Producer = kafka.Producer),
+    (client = new kafka.KafkaClient("127.0.0.1:2181")),
+    (producer = new Producer(client));
+  let count = 0;
+
+  // producer.on("ready", function() {
+  //   let count = 0;
+
+  //   console.log("ready to write message in kafka");
+    let msg = {
+      jobid:jobid,
+      url:url,
+      userName:userName,
+    }
+      payloads = [
+        { topic: "data-retrievalpytest", messages: JSON.stringify(msg), partition: 0, key: "" }
+      ];
+
+      producer.send(payloads, function(err, data) {
+        console.log(data);
+        count += 1;
+      });
+      let msgdt = {
+        jobid:jobid,
+        query:url,
+        userName:userName,
+        status:"DATA_RETRIEVED"
+      } 
+      payloads = [
+        { topic: "SessionManagement", messages: JSON.stringify(msgdt), partition: 0, key: "" }
+      ];
+
+      producer.send(payloads, function(err, data) {
+        console.log(data);
+        count += 1;
+      });
+    res.send();
+  // });
+
+  producer.on("error", function(err) {
+    console.log(err);
   });
 })
-app.get('/logtest',(req,res)=>{
-  console.log("got req")
-})
+.catch(function(error) {
+  console.log(error);
+});
+});
 
-app.get('/climate',(req,res)=>{
-  console.log(req.query.lon+req.query.lat+req.query.appid)
+app.get("/", (req, res) => {
+  console.log("got req11111 empty / test");
+});
+app.post("/login", (req, res) => {
+  var username = req.body.email;
+  var password = req.body.password;
+  console.log("user login "+username + password);
+
+  axios
+    .post("http://localhost:8084/login", {
+      userName: username,
+      password: password
+    })
+    .then(function(response) {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+app.post("/signup", (req, res) => {
+  var username = req.body.email;
+  var password = req.body.password;
+  var name = req.body.name;
+  console.log("user register "+username + password+name);
+
+  axios
+    .post("http://localhost:8084/signup", {
+      userName: username,
+      password: password,
+      name:name,
+    })
+    .then(function(response) {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+app.get("/logtest", (req, res) => {
+  console.log("got req /logtest");
+});
+
+app.get("/climate", (req, res) => {
+  console.log("climate api hit");
+  console.log(req.query.lon + req.query.lat + req.query.appid);
   // request
   // .get('http://localhost:3001:/climate?lon=' + req.query.lon + '&lat=' + req.query.lat + "&appid="+req.query.appid)
   // .on('response', function(response) {
@@ -81,10 +136,16 @@ app.get('/climate',(req,res)=>{
   //   console.log(response.headers['content-type']) // 'image/png'
   // })
   axios
-      .get("http://localhost:3001/climate?lon=" + req.query.lon + "&lat=" + req.query.lat+"&appid=48df9480a2a09854ca753dd0721d3f64")
-      .then(function(response) {
-        console.log(response.data);
-        res.send(response.data);
-      });
-})
+    .get(
+      "http://localhost:3001/climate?lon=" +
+        req.query.lon +
+        "&lat=" +
+        req.query.lat +
+        "&appid=48df9480a2a09854ca753dd0721d3f64"
+    )
+    .then(function(response) {
+      console.log(response.data);
+      res.send(response.data);
+    });
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
