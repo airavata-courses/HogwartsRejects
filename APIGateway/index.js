3adfc83faeed05318965b50286a516ab04fbcce5
Rishabh gajra <rgrishigajra@gmail.com>
2020-02-13 02:04:10 -0500

    api gateway working with Goservice

--- APIGateway/index.js
+++ APIGateway/index.js
@@ -4,6 +4,7 @@ const port = 8090;
 const version = '1.0.0';
 const bp = require('body-parser');
 const apiAdapter = require('./apiAdapter')
+const request = require('request');
 const cors = require('cors')
 const axios = require('axios');
 // const isAuthorized = require('../controller/requestAuthenticator')
@@ -15,9 +16,10 @@ const api = apiAdapter(BASE_URL)
 var kafka = require("kafka-node")
 
 app.post('/dataRetrieval',(req,res)=>{
+
   var url=req.body.fileloc
 
-  console.log(req.body.fileloc)
+  console.log("data retrieval req with url "+req.body.fileloc)
   
   Producer = kafka.Producer,
 
@@ -28,7 +30,7 @@ app.post('/dataRetrieval',(req,res)=>{
 producer.on("ready", function() {
   let count = 0;
 
-  console.log("ready");
+  console.log("ready to write message in kafka");
   setInterval(function() {
     payloads = [
       { topic: "data-retrieval", messages: url, partition: 0, key:"" }
@@ -38,7 +40,8 @@ producer.on("ready", function() {
       console.log(data);
       count += 1;
     });
-  }, 5000);
+  }, 1);
+  res.send()
 });
 
 producer.on("error", function(err) {
@@ -54,6 +57,7 @@ app.post('/login', (req, res) => {
   var username = req.body.email;
   var password = req.body.password;
   console.log(username+password)
+  
   axios
   .post('http://localhost:8084/login',{'userName':username,'password':password})
   .then(function (response) {
@@ -66,5 +70,21 @@ app.post('/login', (req, res) => {
 })
 app.get('/logtest',(req,res)=>{
   console.log("got req")
+})
+
+app.get('/climate',(req,res)=>{
+  console.log(req.query.lon+req.query.lat+req.query.appid)
+  // request
+  // .get('http://localhost:3001:/climate?lon=' + req.query.lon + '&lat=' + req.query.lat + "&appid="+req.query.appid)
+  // .on('response', function(response) {
+  //   console.log(response.statusCode) // 200
+  //   console.log(response.headers['content-type']) // 'image/png'
+  // })
+  axios
+      .get("http://localhost:3001/climate?lon=" + req.query.lon + "&lat=" + req.query.lat+"&appid=48df9480a2a09854ca753dd0721d3f64")
+      .then(function(response) {
+        console.log(response.data);
+        res.send(response.data);
+      });
 })
 app.listen(port, () => console.log(`Listening on port ${port}`));
