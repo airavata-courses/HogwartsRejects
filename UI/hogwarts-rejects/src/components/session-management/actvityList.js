c2fd5e0d86b82d76355c0e41c4f5a0b46cf75726
Gopi Kiran <gkiran@iu.edu>
2020-02-13 21:41:54 -0500

    cleaning branch

--- UI/hogwarts-rejects/src/components/session-management/actvityList.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-function ActivityList(){
-    return(
-        <div>
-            <h1>Your Past Activities appear here</h1>
-        </div>
-    )
-}
-export default ActivityList
