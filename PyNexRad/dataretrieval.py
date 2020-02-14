745037855b1c7e743da892e9020eca9e13eb9e8b
Gopi Kiran <gkiran@iu.edu>
2020-02-13 21:39:27 -0500

    cleaning masgter

--- PyNexRad/dataretrieval.py
+++
@@ -1,6 +0,0 @@
-from pykafka import KafkaClient
-client = KafkaClient(hosts="127.0.0.1:9092")
-topic = client.topics['modeling-service']
-producer = topic.get_sync_producer()
-consumer = topic.get_simple_consumer()
-producer.produce(b"2019/06/26/KVWX/KVWX20190626_221105_V06")
