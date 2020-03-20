5f47f099673876918056a550973df78af60a1014
Vivek Shresta Bandaru <vivekshrestabandaru@gmail.com>
2020-03-20 11:25:48 -0400

    Testing docker build

--- PostProcessor/src/main/java/org/iu/ads/processor/PostProcessApplication.java
+++ PostProcessor/src/main/java/org/iu/ads/processor/PostProcessApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class PostProcessApplication {
 
     public static void main(String[] args) {
