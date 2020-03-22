4956a884c6f6e41b5b111437ee1f742c4c84f8fd
Gopi Kiran <gkiran@iu.edu>
2020-03-21 21:49:41 -0400

    correcting user-management master

---
+++ UserManagement/src/main/java/org/iu/ads/service/HttpAPI.java
@@ -0,0 +1,95 @@
+package org.iu.ads.service;
+
+import org.iu.ads.repository.UserRepository;
+import org.iu.ads.utility.JwtUtility;
+import org.iu.ads.utility.PostBodyParser;
+import org.iu.ads.view.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HttpAPI {
+
+    public static final String JWT_TOKEN = "jwtToken";
+
+    @Autowired
+    public UserRepository userRepo;
+
+    @Autowired
+    public PostBodyParser postBodyParser;
+
+    @Autowired
+    public JwtUtility jwtUtility;
+
+    @RequestMapping(value = "/login")
+    public ResponseEntity<?> login(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, String> result = new HashMap<>();
+        Map<String, String> postBody = postBodyParser.getPostBodyInAMap(request);
+
+        String userName = postBody.get("userName");
+        String password = postBody.get("password");
+
+        for(UserInfo userInfo : userRepo.findByUserName(userName)) {
+            if(userInfo.getPassword().equals(password)) {
+                result.put(JWT_TOKEN, jwtUtility.generateToken(userInfo));
+                result.put("fullName", userInfo.getFullName());
+                break;
+            }
+        }
+
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping(value = "/verify")
+    public ResponseEntity<?> verify(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, String> result = new HashMap<>();
+        Map<String, String> postBody = postBodyParser.getPostBodyInAMap(request);
+
+        String jwt = postBody.get("jwt").substring(7);
+        String userName = jwtUtility.extractUserName(jwt);
+
+        if(userRepo.findByUserName(userName).isEmpty()) {
+            return ResponseEntity.ok(result);
+        }
+
+        result.put("userName", userName);
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping(value = "/signup")
+    public ResponseEntity<?> signup(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, String> result = new HashMap<>();
+
+        Map<String, String> postBody = postBodyParser.getPostBodyInAMap(request);
+
+        String userName = postBody.get("userName");
+        String password = postBody.get("password");
+        String fullName = postBody.get("fullName");
+
+        UserInfo userInfo = new UserInfo(userName, password, fullName);
+
+        if(!userRepo.findByUserName(userName).isEmpty()) {
+            return ResponseEntity.ok(result);
+        }
+
+        userRepo.save(userInfo);
+
+        result.put(JWT_TOKEN, jwtUtility.generateToken(userInfo));
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping(value = "/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, String> result = new HashMap<>();
+        result.put("jwt", "");
+
+        return ResponseEntity.ok(result);
+    }
+}
---
+++ UserManagement/src/test/java/org/iu/ads/filter/TestCorsFilter.java
@@ -0,0 +1,28 @@
+package org.iu.ads.filter;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestCorsFilter {
+
+    @Test
+    public void testAllowedDomains() {
+        String requestDomain = "http://localhost:3000";
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+
+        Mockito.when(request.getHeader("Origin")).thenReturn(requestDomain);
+
+        try {
+            new CORSFilter().doFilter(request, response, Mockito.mock(FilterChain.class));
+            Mockito.verify(response).addHeader("Access-Control-Allow-Origin", requestDomain);
+        } catch (Exception e) {
+            Assert.fail();
+        }
+    }
+}
