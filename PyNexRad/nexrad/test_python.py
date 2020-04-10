7fce3e2ce38ea17e0884732c28e42e593bcf39ad
Rishabh Gajra <rgrishigajra@gmail.com>
2020-04-09 20:18:53 -0400

    Update test_python.py

--- PyNexRad/nexrad/test_python.py
+++ PyNexRad/nexrad/test_python.py
@@ -3,7 +3,7 @@ import plot_graph
 import pickle
  
 def test_py():
-    with open('mock.data', 'rb') as mock_data:
+    with open('./mock.data', 'rb') as mock_data:
          [az,ref, rho, zdr, phi,ref_range, rho_range,zdr_range, phi_range, fdate] = pickle.load(mock_data)
     assert isinstance(plot_graph.plot_data(az,ref, rho, zdr, phi,ref_range, rho_range,zdr_range, phi_range, fdate),str)
 def test_raises_except():
